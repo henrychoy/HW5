97053bdddd63aedbdee02106a96f276875f4d8aa
Choy <hchoy@partners.org>
2020-12-02 23:39:13 -0500

    working solution

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 /*jshint esversion: 6 */
 
+const displayContainer = document.getElementById("display");
 document.getElementById("btn").addEventListener('click', loadDegrees);
 
 function loadDegrees() {
@@ -9,16 +10,19 @@ function loadDegrees() {
     request.onload = function () {
         if (request.status === 200) {
             const data = JSON.parse(request.responseText);
-            const degree = data.my_college_degrees;
-            console.log(degree);
 
 
-            let output='';
-            for(let i=0; i < degree.length; i++){
-                output += '<li>'+JSON.stringify(degree[i])+'</li>'
+            let htmlString = "";
+            for (let i=0; i < data.my_college_degrees.length; i++){
+                htmlString += "<p>School: " + data.my_college_degrees[i].School + "<br>" +
+                    "Major: " + data.my_college_degrees[i].Major + "<br>" +
+                        "Type: " + data.my_college_degrees[i].Type + "<br>" +
+                        "Year Complete: " + data.my_college_degrees[i].Year_Complete + "<br><br></p>";
             }
-            document.getElementById("display").innerHTML = output;
 
+
+            displayContainer.insertAdjacentHTML("beforeend", htmlString);
+            document.getElementById("btn").disabled = true;
         }
     }
     request.send();
