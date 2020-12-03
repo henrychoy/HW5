8abc5a266b689171f1b47400b212ccf53bc11eba
Choy <hchoy@partners.org>
2020-12-02 21:49:48 -0500

    added code

--- javascript.js
+++ javascript.js
@@ -1,12 +1,33 @@
 /*jshint esversion: 6 */
 
+document.getElementById("btn").addEventListener('click', loadDegrees);
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://henrychoy.github.io/HW5/degrees.json');
-request.onload = function () {
-    const data = JSON.parse(request.responseText);
-    console.log(data[0]);
+function loadDegrees() {
+    const request = new XMLHttpRequest();
+    request.open('GET', 'degrees.json', true);
+
+    request.onload = function () {
+        if (request.status === 200) {
+            const data = JSON.parse(request.responseText);
+            const degree = data.my_college_degrees;
+            console.log(degree);
+
+
+            let output='';
+            for(let i=0; i < degree.length; i++){
+                output += '<li>'+JSON.stringify(degree[i])+'</li>'
+            }
+            document.getElementById("display").innerHTML = output;
+
+        }
+    }
+    request.send();
+
 }
 
 
-request.send();
+
+
+
+
+
