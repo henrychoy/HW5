01fbb632f8924f356a83525789383340f1955fc1
Choy <hchoy@partners.org>
2020-12-02 21:51:54 -0500

    added JSON url

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ document.getElementById("btn").addEventListener('click', loadDegrees);
 
 function loadDegrees() {
     const request = new XMLHttpRequest();
-    request.open('GET', 'degrees.json', true);
+    request.open('GET', 'https://henrychoy.github.io/HW5/degrees.json', true);
 
     request.onload = function () {
         if (request.status === 200) {
